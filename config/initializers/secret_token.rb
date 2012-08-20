41870a09a0d1d71222e28016790840c574d3081b
Chris Wenner <crwenner@gmail.com>
2012-08-19 21:51:48 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeekArc::Application.config.secret_token = '909ff070e721ceed3729669435bbed591d6f6f322159147aa4af4e4e05cef2180dfddf2b786f270109edf7e32fdc3b0b43dc873608289451fae5225a9883f2b8'
