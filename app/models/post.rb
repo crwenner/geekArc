5ccf6277c7af53d497b97b20ff2de7e7d5fd88ee
Keith Thompson <keith@brilliantfantastic.com>
2012-08-23 23:41:47 -0400

    Removed the previous changes, still need to implement the markdown code

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,24 +2,4 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :date, :title
 
   has_and_belongs_to_many :tags
-
-  def to_html
-    highlight(markdown.render(self.content))
-  end
-
-  private
-
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
-  options = {
-    :autolink => true,
-    :no_intra_emphasis => true,
-    :fenced_code_blocks => true
-  }
-
-  def highlight(html)
-    doc.search("//pre[@lang]").each do |pre|
-      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
-    end
-    doc.to_s
-  end
 end
