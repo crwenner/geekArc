f7d4759f883cb2302726ba22a38afe57c57ebb08
Keith Thompson <keith@brilliantfantastic.com>
2012-09-04 22:04:50 -0400

    Added markdown helper method for rendering markdown as html in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def markdown(text)
+    options = {
+      :no_intra_emphasis => true,
+      :fenced_code_blocks => true,
+      :autolink => true
+    }
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(:filter_html => true, :hard_wrap => true), options)
+    syntax_highlighter(markdown.render(text)).html_safe
+  end
+
+  def syntax_highlighter(html)
+    doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s
+  end
 end
